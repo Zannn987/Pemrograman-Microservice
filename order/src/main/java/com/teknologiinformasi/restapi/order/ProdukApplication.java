60a9ce788d5280afbf127201caefe864d5f92116
Maulana Hazanni Akbar <mhazanni123@gmail.com>
2025-03-05 11:47:38 +0700

    membuat Synchronous

---
+++ order/src/main/java/com/teknologiinformasi/restapi/order/ProdukApplication.java
@@ -0,0 +1,20 @@
+package com.teknologiinformasi.restapi.order;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+public class ProdukApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProdukApplication.class, args);
+	}
+
+	@Bean
+	public RestTemplate restTemplate() {
+		return new RestTemplate();
+	}
+
+}
